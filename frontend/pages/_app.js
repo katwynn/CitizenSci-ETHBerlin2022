33043245756d7b0e22a0651d3559a886342dddf4
aurora-721 <aurora.makovac@gmail.com>
2022-09-18 07:13:45 +0200

    readme

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -3,9 +3,11 @@ import '../styles/globals.css';
 
 import "@rainbow-me/rainbowkit/styles.css";
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+
+import { chain, configureChains, createClient, useSigner, WagmiConfig } from "wagmi";
 import { infuraProvider } from "wagmi/providers/infura";
 import { publicProvider } from "wagmi/providers/public";
+import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
 
 const infuraId = process.env.NEXT_PUBLIC_INFURA_ID;
 
@@ -25,12 +27,19 @@ const wagmiClient = createClient({
   provider,
 });
 
+
+// In this example, we'll use the Ethereum Goerli Testnet
+const desiredChainId = ChainId.Mumbai;
+
+
 function MyApp({ Component, pageProps }) {
   return (
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains}>
+      <ThirdwebProvider desiredChainId={desiredChainId}>
           <Navbar />
           <Component {...pageProps} />
+        </ThirdwebProvider>
       </RainbowKitProvider>
     </WagmiConfig>
   );
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,7 +2,24 @@ import Head from 'next/head'
 import Main from '../components/Main'
 import Projects from '../components/Projects'
 
+
+import {
+  useClaimedNFTSupply,
+  useContractMetadata,
+  useUnclaimedNFTSupply,
+  useActiveClaimCondition,
+  Web3Button,
+  useContract,
+} from "@thirdweb-dev/react";
+
+import { useAccount, useDisconnect } from "wagmi";
+
+
+
 export default function Home() {
+  const myNftDropContractAddress = "0x2EA0AcB4854E0Ad6b2306b824Cbae2560D2Bf06e"
+  const { contract: nftDrop } = useContract(myNftDropContractAddress);  const { address } = useAccount();
+
   return (
     <div>
       <Head>
@@ -11,6 +28,26 @@ export default function Home() {
         <link rel="icon" href="/fav.png" />
       </Head>
       <Main />
+      <Web3Button
+        contractAddress={myNftDropContractAddress}
+        action={async (contract) =>
+          await contract.erc721.claim(quantity)
+        }
+        // If the function is successful, we can do something here.
+        onSuccess={(result) =>
+          alert(
+            `Successfully minted ${result.length} NFT${
+              result.length > 1 ? "s" : ""
+            }!`
+          )
+        }
+        // If the function fails, we can do something here.
+        onError={(error) => alert(error?.message)}
+        accentColor="#f213a4"
+        colorMode="dark"
+      >
+        Mint 
+        </Web3Button>
       <Projects />
     </div>
   )
